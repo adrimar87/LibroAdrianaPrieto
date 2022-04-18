5022059d2188c7028d04146e42a79d6785385072
adrimar87 <91295306+adrimar87@users.noreply.github.com>
2022-04-18 17:55:49 -0300

    modif componentes

--- Navbar/src/App.js
+++ Navbar/src/App.js
@@ -1,18 +1,33 @@
-import logo from './LogoL.svg';
+
 import './App.css';
 import Menu from './components/Navbar/Menu';
+import CartWidget from './components/Cart/CartWidget';
+import carito from './imagenes/carrito.png'
+import logo from './imagenes/LogoL.svg';
+import ItemListContainer from './components/Item/ItemListContainer';
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" /> 
-        <Menu/>
+             
+          <img src={logo} className="App-logo" alt="logo" style={{width:"130px"}} />   
+        
+           <Menu/> 
+           <CartWidget imagen={carito} />
        
       </header>
-     
+      <div className="App-items">
+            <ItemListContainer/>
+            
+      </div> 
     </div>
+    
+      
+     
+
   );
 }
 
--- Navbar/src/components/Navbar/Menu.jsx
+++ Navbar/src/components/Navbar/Menu.jsx
@@ -1,19 +1,46 @@
-import React from "react"
+import React from 'react'
 
 
-export default function menu()
-{
-  return( 
-    <nav className="App-link" >
-     <form className="form-inline" >
-        <button  type="button" className="btn btn-info">Libros</button>
-        <button className="btn-group btn-group-lg" type="button">Comentarios</button>
-        <button className="btn btn-sm btn-outline-secondary" type="button">Contacto</button>
-       
-      </form>
-    </nav>
+const Menu = () => {
+  return (
+    
+    
+    <div>
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Libros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Comentarios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Contactanos</a>
+            </li>
+          </ul>
+              
+        {/* <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="#">Inicio </a>
+            <a className="nav-item nav-link" href="#">Libros</a>
+            <a className="nav-item nav-link" href="#">Comentarios</a>
+            <a className="nav-item nav-link" href="#">Contactos</a>
 
-    );
-   
- 
+            
+          </div>
+        </div>
+      </nav> */}
+    </div>
+    
+  )
 }
+
+export default Menu
